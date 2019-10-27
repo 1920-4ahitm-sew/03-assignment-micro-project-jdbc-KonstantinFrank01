53fec28626288578f41ed2c66eade551ab59d11b
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-10-27 09:49:18 +0100

    created the RestConfig class

---
+++ src/main/java/at/htl/footballleague/boundary/RestConfig.java
@@ -0,0 +1,13 @@
+package at.htl.footballleague.boundary;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestConfig extends Application {
+
+    public RestConfig() {
+
+    }
+
+}
