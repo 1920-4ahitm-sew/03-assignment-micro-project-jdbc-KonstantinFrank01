c431f9041f4c55410be9a66d0277eb9ce237f974
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-10-27 11:03:34 +0100

    implemented the GET-Method in the Endpoint

--- src/main/java/at/htl/footballleague/boundary/FootballLeagueEndpoint.java
+++ src/main/java/at/htl/footballleague/boundary/FootballLeagueEndpoint.java
@@ -1,4 +1,27 @@
 package at.htl.footballleague.boundary;
 
+import at.htl.footballleague.entity.Player;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Stateless
+@Path("/")
 public class FootballLeagueEndpoint {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Player getPlayer(@PathParam("id") long id) {
+        return em.find(Player.class, id);
+    }
 }
