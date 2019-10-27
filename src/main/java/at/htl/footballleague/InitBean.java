3c154ad48308af7d359480fa06c99c0bd3ec11fd
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-10-27 11:20:46 +0100

    implemented players in InitBean

--- src/main/java/at/htl/footballleague/InitBean.java
+++ src/main/java/at/htl/footballleague/InitBean.java
@@ -1,5 +1,7 @@
 package at.htl.footballleague;
 
+import at.htl.footballleague.entity.Player;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Destroyed;
 import javax.enterprise.context.Initialized;
@@ -17,9 +19,14 @@ public class InitBean {
 
     public void init(@Observes @Initialized(ApplicationScoped.class) Object init) {
         System.out.println("*** it works ***");
+
+        Player messi = new Player("Lionel", "Messi", (long) 3, 10);
+        em.persist(messi);
+        em.persist(new Player("Frenkie", "De Jong", (long) 3, 21));
+        em.persist(new Player("Gianluigi", "Buffon", (long) 5, 1));
     }
 
     public void tearDown(@Observes @Destroyed(ApplicationScoped.class) Object init) {
-
+        //when app is undeployed
     }
 }
